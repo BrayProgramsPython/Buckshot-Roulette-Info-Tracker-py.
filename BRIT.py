b3f2aa4c04d39405b00cf2e539b2b726b8024fe4
BrayProgramsPython <braystr6514@gmail.com>
2026-08-31 20:04:13 -0400

    BRIT.py

--- BRIT.py
+++ BRIT.py
@@ -1,14 +1,20 @@
+#Program to keep track of information in BR
+
 while True:
+#Loops program for game ends/starts
     print("NEW GAME")
     numRounds = int(input("Enter num of rounds (1-3): "))
     currentRound = 1
+    #Dictates when the program will loop back to "NEW GAME" based on user input
 
     setup_Left = int(input("Type 0 for player left or 1 to skip: "))
     setup_Right = int(input("Type 0 for player right or 1 to skip: "))
     setup_Across = int(input("Type 0 for player across or 1 to skip: "))
+    #dictates what players will be displayed on info block and counted towards round endings bsaed on user input
     
     while currentRound <= numRounds:
         print("Round",currentRound,"/",numRounds)
+        #loops the rounds based on when user health or enemy healths hit 0, dictates return to "NEW GAME"
         
         player_Left = 0
         player_Right = 0
@@ -22,19 +28,23 @@ while True:
             player_Right += maxHP
         if setup_Across == 0:
             player_Across += maxHP
+            #tracks max hp for the current round for whatever players were entered above
             
         while True:
             rise = 1
-            ask_burner = True
             print("RELOAD")
+            #loops questions for info based on shell count left, starts rise counter and enables burner question
             live = int(input("Enter num live: "))
             blank = int(input("Enter num blank: "))
             fall = live + blank
+            ask_burner = True
+            #enters number of lives and blanks, starts fall counter based on total shells
             while fall > 10 or fall < 1:
                 print("Must be 1-10: ")
                 live = int(input("Enter num live: "))
                 blank = int(input("Enter num blank: "))
                 fall = live + blank
+                #error trap for previous block
 
             print("")
             print("--------------------")
@@ -52,10 +62,12 @@ while True:
             print("Self HP:",self)
             print("--------------------")
             print("")
+            #info block dispalying shell types, counts, numbers, and hps,
 
             while live != 0 or blank != 0:
                 if fall < 1:
                     break
+                    #loops questions for each players turn, based on shell counts
 
                 leave = input("Did anyone leave? (left, right, across, no): ")
                 if leave == 'left':
@@ -71,6 +83,7 @@ while True:
                     print("GAME END - NOT ENOUGH PLAYERS")
                     break
                     break
+                    #Checks if anyone leaves, if so removes them from counters, ends game if all enemies leave
                 
                 racked = True
                 while racked == True:
@@ -103,25 +116,28 @@ while True:
                             print("ENTER LEFT, RIGHT, ACROSS, SELF, OR NO")
                         elif heal == 'no':
                             keep_healing = False
-                    
-                    if ask_burner:
-                        burnNum = int(input("Enter burner shell num or 0 to skip: "))
-                        if burnNum >=1 and burnNum <= 10:
-                            burnShell = input("Enter burner shell type: ")
-                            ask_burner = False
-                        elif burnNum < 0 or burnNum > 10:
-                            burnNum = int(input("Enter burner shell NUM or ZERO to skip: "))
-                            burnShell = input("Enter burner shell type (live/blank): ")
-                            ask_burner = False
+                            #Loops healing incase someone heals multiple times, prevents heal input if player is full hp or not in the game, error traps bad input, and ends loop when "no"
+                    if fall > 2:
+                        if ask_burner:
+                            burnNum = int(input("Enter burner shell num or 0 to skip: "))
+                            if burnNum >=1 and burnNum <= 10:
+                                burnShell = input("Enter burner shell type: ")
+                                ask_burner = False
+                                #enters burner number and type, registers for later info display
+                            elif burnNum < 0 or burnNum > 10:
+                                burnNum = int(input("Enter burner shell NUM or ZERO to skip: "))
+                                burnShell = input("Enter burner shell type (live/blank): ")
+                                ask_burner = False
+                                #error trap for previous block
                         
                     fired_racked = input("Type f for fired or r for racked: ")
                     while True:
                         fire = input("enter shell fired/racked (live/blank): ")
-                        if fire == 'live' and live <= 0:
+                        if fire == 'live' and live == 0:
                             print("Live already zero, entering blank")
                             fire = 'blank'
                             break
-                        elif fire == 'blank' and blank <= 0:
+                        elif fire == 'blank' and blank == 0:
                             print("Blank already zero, entering live")
                             fire = 'live'
                             break
@@ -129,6 +145,7 @@ while True:
                             break
                         elif fire not in ['live', 'blank']:
                             print("Enter LIVE or BLANK")
+                        
                     if fire == 'live' and fired_racked == 'f':
                         racked = False
                         live -= 1
@@ -137,6 +154,15 @@ while True:
                         live_inv = int(input("Type 1 for live inverted or 0 for no invert: "))
                         if live_inv == 0:
                             target = input("Enter targeted player (left, right, across, self): ")
+                            if target == 'left' and player_Left == 0:
+                                    print ("Player Left already dead")
+                                    target = input("Enter targeted player (left, right, across, self): ")
+                            elif target == 'right' and player_Right == 0:
+                                    print ("Player Right already dead")
+                                    target = input("Enter targeted player (left, right, across, self): ")
+                            elif target == 'across' and player_Across == 0:
+                                    print ("Player Across already dead")
+                                    target = input("Enter targeted player (left, right, across, self): ")
                             damage = int(input("Type 1 for single damage. 2 for double damage: "))
                             if target == 'left':
                                 player_Left -= damage
@@ -146,6 +172,7 @@ while True:
                                 player_Across -= damage
                             elif target == 'self':
                                 self -= damage
+                                #checks if fired or racked, ensures correct type is entered and error traps bad input, if live, adjusts counter and checks for invert. Then asks for target and damage, adjusts target health, and asks again if target already dead
                         elif live_inv == 1:
                             skip = input("Was blank shot at shooter? (y/n): ")
                             if skip == 'y' and live == 0 and blank == 0:
@@ -165,6 +192,7 @@ while True:
                                 print("--------------------")
                                 print("")
                                 racked = True
+                                #if live inverted, checks for turn skip and breaks loop if gun empty, then prints smaller info block because of no damage
                     elif fire == 'blank' and fired_racked == 'f':
                         racked = False
                         blank -= 1
@@ -182,6 +210,7 @@ while True:
                                 player_Across -= damage
                             elif target == 'self':
                                 self -= damage
+                                #check if blank is fired, adjusts counters and check for inversion. If inverted, gets target and damage and adjusts health
                         elif blank_inv == 0:
                             skip = input("Was blank shot at shooter? (y/n): ")
                             if skip == 'y' and live == 0 and blank == 0:
@@ -201,6 +230,7 @@ while True:
                                 print("--------------------")
                                 print("")
                                 racked = True
+                                #checks if blank fired, asks if shot at shooter for turn skip, breaks loop to return to questions and prints small info block because of no damage
                     elif fire == 'live' and fired_racked == 'r':
                         live -= 1
                         rise += 1
@@ -219,6 +249,7 @@ while True:
                             print("Shell",burnNum,burnShell,"50/50")
                         print("--------------------")
                         print("")
+                        #checks if live racked, if so adjusts counters and prints small info block because no damage
                     elif fire == 'blank' and fired_racked == 'r':
                         blank -= 1
                         rise += 1
@@ -237,6 +268,7 @@ while True:
                             print("Shell",burnNum,burnShell,"50/50")
                         print("--------------------")
                         print("")
+                        #checks if blank and racked, if so adjusts counters and prints small info block because no damage
 
                 print("")
                 print("--------------------")
@@ -259,13 +291,17 @@ while True:
                 print("Self HP:",self)
                 print("--------------------")
                 print("")
+                #full info block printed when someone takes damage. Displays shell count, type, number, burner info, and hps
 
                 if self <= 0:
                     print("YOU DIED - ROUND OVER")
                     break
+                    #checks if user dies to loop to next round
                 if player_Left == 0 and player_Right == 0 and player_Across == 0:
                     print("ROUND WON")
                     break
+                    #checks if enemies die to loop to next round
             if self == 0 or (player_Left == 0 and player_Right == 0 and player_Across == 0):
                 currentRound += 1
                 break
+                #initiates the loop break for new rounds based on previous 2 blocks
